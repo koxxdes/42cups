54aa30628b08cf66fab046dcb51cf70ce1fe0ef3
Victor <koxxdes@gmail.com>
2014-04-18 21:48:18 +0300

    ticket:1 UI for user info

--- KavProject/KavProject/FirstViewController.m
+++ KavProject/KavProject/FirstViewController.m
@@ -9,10 +9,17 @@
 #import "FirstViewController.h"
 
 @interface FirstViewController ()
+@property (weak, nonatomic) IBOutlet NSLayoutConstraint *textFieldWidthConstraint;
+@property (weak, nonatomic) IBOutlet NSLayoutConstraint *labelWidthConstraint;
+@property (weak, nonatomic) IBOutlet NSLayoutConstraint *labeToTextFieldSpacingConstraint;
+@property (weak, nonatomic) IBOutlet NSLayoutConstraint *bioTextViewWidthConstraint;
 
 @end
 
-@implementation FirstViewController
+@implementation FirstViewController{
+    float textFieldWidth;
+    float textViewWidth;
+}
 
 - (void)viewDidLoad
 {
@@ -24,6 +31,23 @@
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+-(void)willAnimateRotationToInterfaceOrientation:(UIInterfaceOrientation)toInterfaceOrientation duration:(NSTimeInterval)duration
+{
+    [super willAnimateRotationToInterfaceOrientation:toInterfaceOrientation duration:duration];
+    
+    if (UIInterfaceOrientationIsLandscape(toInterfaceOrientation)) {
+        textFieldWidth = self.textFieldWidthConstraint.constant;
+        textViewWidth = self.bioTextViewWidthConstraint.constant;
+        
+        self.textFieldWidthConstraint.constant = self.view.frame.size.width - self.labelWidthConstraint.constant - self.labeToTextFieldSpacingConstraint.constant - 2*40;
+        self.bioTextViewWidthConstraint.constant = self.view.frame.size.width - 2*40;
+    }else
+    {
+        self.textFieldWidthConstraint.constant = textFieldWidth;
+        self.bioTextViewWidthConstraint.constant = textViewWidth;
+    }
 }
 
 @end
