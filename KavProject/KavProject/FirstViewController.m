1245955b4b113983c120ec48087b0b05267b0a82
Victor <koxxdes@gmail.com>
2014-04-18 19:44:14 +0300

    initial commit
    empty tabbed app

---
+++ KavProject/KavProject/FirstViewController.m
@@ -0,0 +1,29 @@
+//
+//  FirstViewController.m
+//  KavProject
+//
+//  Created by Victor on 4/18/14.
+//  Copyright (c) 2014 Victor. All rights reserved.
+//
+
+#import "FirstViewController.h"
+
+@interface FirstViewController ()
+
+@end
+
+@implementation FirstViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
